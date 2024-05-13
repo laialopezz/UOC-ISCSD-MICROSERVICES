d7de18daaa7386ca34297c5790987bddb7a505df
Laia <laialopezz99@gmail.com>
2024-05-13 18:31:51 +0200

    [Logic] getProductsByName + getProductsByCategoryId

--- product-catalog/src/main/java/edu/uoc/epcsd/productcatalog/controllers/ProductController.java
+++ product-catalog/src/main/java/edu/uoc/epcsd/productcatalog/controllers/ProductController.java
@@ -1,8 +1,8 @@
 package edu.uoc.epcsd.productcatalog.controllers;
 
-
 import edu.uoc.epcsd.productcatalog.controllers.dtos.CreateProductRequest;
 import edu.uoc.epcsd.productcatalog.controllers.dtos.GetProductResponse;
+import edu.uoc.epcsd.productcatalog.entities.Category;
 import edu.uoc.epcsd.productcatalog.entities.Product;
 import edu.uoc.epcsd.productcatalog.services.ProductService;
 import lombok.extern.log4j.Log4j2;
@@ -15,6 +15,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Log4j2
 @RestController
@@ -37,7 +39,8 @@ public class ProductController {
     public ResponseEntity<GetProductResponse> getProductById(@PathVariable @NotNull Long productId) {
         log.trace("getProductById");
 
-        return productService.findById(productId).map(product -> ResponseEntity.ok().body(GetProductResponse.fromDomain(product)))
+        return productService.findById(productId)
+                .map(product -> ResponseEntity.ok().body(GetProductResponse.fromDomain(product)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
@@ -63,7 +66,34 @@ public class ProductController {
 
     // TODO: add the code for the missing system operations here:
     // 1. remove product (use DELETE HTTP verb). Must remove the associated items
-    // 2. query products by name
-    // 3. query products by category/subcategory
+    // Cambiar las unidades del producto al estado NOT_OPERACIONAL
+
+    @GetMapping("/getByName/{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getProductsByName(@PathVariable @NotNull String name) {
+        log.trace("getProductsByName");
+        Stream<Product> categories = productService.findAll().stream()
+                .filter((product -> product.getName().equals(name)));
+        return categories.collect(Collectors.toList());
+    }
+
+    @GetMapping("/getByCategoryId/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getProductsByCategoryId(@PathVariable @NotNull Long id) {
+        log.trace("getProductsByCategoryId");
+        List<Product> categories = List.of();
+        productService.findAll().forEach(product -> {
+            if (product.getCategory().getId().equals((id))) {
+                categories.add(product);
+            } else {
+                product.getCategory().getChildren().forEach(subcategory -> {
+                    if (subcategory.getId().equals((id)))
+                        categories.add(product);
+                });
+            }
+        });
+
+        return categories;
+    }
 
 }
